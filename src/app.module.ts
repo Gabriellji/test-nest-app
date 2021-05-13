78c31ec14c3edf24d08b1b98594d03a52d0587f8
Gabriellji <gabriellji@yahoo.com>
2021-05-13 16:59:35 +0300

    feat: test nonclass providers + factory providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot()],
+  imports: [CoffeesModule, TypeOrmModule.forRoot(), CoffeeRatingModule],
   controllers: [AppController],
   providers: [AppService],
 })
