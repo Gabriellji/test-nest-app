9a93d2aaf89aace0a242c45adb2570fd98f0f2bd
Gabriellji <gabriellji@yahoo.com>
2021-05-13 17:17:33 +0300

    feat: test dynamic module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot(), CoffeeRatingModule],
+  imports: [CoffeesModule, TypeOrmModule.forRoot(), CoffeeRatingModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
